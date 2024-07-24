ba6a4f0c96be8ef975dd1e05b6efd2b2e22ddf65
Olivier <oliviernadeau2003@gmail.com>
2024-07-24 00:28:11 -0400

    initial commit

---
+++ ProjectInstaller.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace PoolCleaner
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur de composants
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serviceProcessInstaller1 = new System.ServiceProcess.ServiceProcessInstaller();
+            this.serviceInstaller1 = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // serviceProcessInstaller1
+            // 
+            this.serviceProcessInstaller1.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+            this.serviceProcessInstaller1.Password = null;
+            this.serviceProcessInstaller1.Username = null;
+            // 
+            // serviceInstaller1
+            // 
+            this.serviceInstaller1.Description = "Pool Cleaner Service By Olivier Nadeau";
+            this.serviceInstaller1.DisplayName = "Pool Cleaner";
+            this.serviceInstaller1.ServiceName = "Pool Cleaner";
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.serviceProcessInstaller1,
+            this.serviceInstaller1});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller serviceProcessInstaller1;
+        private System.ServiceProcess.ServiceInstaller serviceInstaller1;
+    }
+}
---
+++ Service1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.IO;
+using Microsoft.Win32;
+
+namespace PoolCleaner
+{
+    public partial class Service1 : ServiceBase
+    {
+        private const int timerInterval = 5000;                            // milliseconds (86400000 -> 24 hour)
+        private readonly Timer timer = new Timer();                        // C:\Windows\System32\spool\PRINTERS\
+        private readonly DirectoryInfo workingDirectory = new DirectoryInfo("C:\\Windows\\System32\\spool\\PRINTERS\\");
+        private readonly string logsPath = AppDomain.CurrentDomain.BaseDirectory + "\\logs";
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            WriteToFile("Service is started at " + DateTime.Now);
+            timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+            timer.Interval = timerInterval; 
+            timer.Enabled = true;
+        }
+
+        protected override void OnStop()
+        {
+            WriteToFile("Service is started at " + DateTime.Now);
+        }
+
+        // When Timer Is Reached, It Execute This Code
+        private void OnElapsedTime(object source, ElapsedEventArgs e)
+        {
+            int nbDeletedFile = 0;
+            foreach (FileInfo file in workingDirectory.GetFiles())
+            {
+                file.Delete();
+                nbDeletedFile++; 
+            }
+            WriteToFile("Service is recall at " + DateTime.Now);
+            WriteToFile("# Service Deleted " + nbDeletedFile + " Files --\n" );
+        }
+
+        private void WriteToFile(string message)
+        {
+            if (!Directory.Exists(logsPath))
+                Directory.CreateDirectory(logsPath);
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\ServiceLog_" + DateTime.Now.ToShortDateString().Replace('/', '_') + ".txt";
+            if (!File.Exists(filepath))
+                using (StreamWriter sw = File.CreateText(filepath)) { sw.WriteLine(message); }
+            else
+                using (StreamWriter sw = File.AppendText(filepath)) { sw.WriteLine(message); }
+        }
+    }
+}
